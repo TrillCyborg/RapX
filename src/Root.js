a8ea2ed86fe0457695490af2f8a848dfac7be962
Jason Werner <jasondwerner@gmail.com>
2016-09-30 18:14:32 +0300

    add and init firebase

--- src/Root.js
+++ src/Root.js
@@ -1,29 +1,42 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
 import { Provider } from 'react-redux';
+import firebase from 'firebase';
 import Store from './store/Store';
 
-const Root = () => {
-  const { container, welcome, instructions } = styles;
-  return (
-    <Provider store={Store}>
-      <View style={container}>
-        <Text style={welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    </Provider>
-  );
-};
+class Root extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyBwTvt6DpDCcqlPaAN3ETqxpsFFhL8MlHk',
+      authDomain: 'rapx-4dfa8.firebaseapp.com',
+      databaseURL: 'https://rapx-4dfa8.firebaseio.com',
+      storageBucket: 'rapx-4dfa8.appspot.com',
+      messagingSenderId: '683961390118',
+    });
+  }
 
-const styles = StyleSheet.create({
+  render() {
+    const { container, welcome, instructions } = styles;
+    return (
+      <Provider store={Store}>
+        <View style={container}>
+          <Text style={welcome}>
+            Welcome to React Native!
+          </Text>
+          <Text style={instructions}>
+            To get started, edit index.ios.js
+          </Text>
+          <Text style={instructions}>
+            Press Cmd+R to reload,{'\n'}
+            Cmd+D or shake for dev menu
+          </Text>
+        </View>
+      </Provider>
+    );
+  }
+}
+
+const styles = {
   container: {
     flex: 1,
     justifyContent: 'center',
@@ -40,6 +53,6 @@ const styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
-});
+};
 
 export default Root;
