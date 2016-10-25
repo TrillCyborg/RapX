d49d92cdfa52ca14cc00729c145b9ab2e1267f8c
Jason Werner <jasondwerner@gmail.com>
2016-10-25 23:40:42 +0200

    setup webRTC

--- src/components/Battle.js
+++ src/components/Battle.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Text } from 'react-native';
+import { getUserMedia } from 'react-native-webrtc';
 import ScreenContainer from './ScreenContainer';
+import { Button } from './Input';
 
-const Battle = () => (
-  <ScreenContainer center>
-    <Text>Battle</Text>
-  </ScreenContainer>
-);
+class Battle extends Component {
+  constructor() {
+    super();
+    this.state = {
+      streamURL: '',
+    };
+    this.getLocalStream = this.getLocalStream.bind(this);
+  }
+
+  getLocalStream() {
+    getUserMedia({
+      audio: true,
+      video: false,
+    }, (stream) => {
+      console.log('THE STREAM', stream);
+      this.setState({ streamURL: stream.toURL() });
+    }, (error) => {
+      console.log('STREAM ERROR', error);
+    });
+  }
+
+  render() {
+    return (
+      <ScreenContainer center>
+        <Text>Battle</Text>
+        <Button onPress={this.getLocalStream}>Start Stream</Button>
+      </ScreenContainer>
+    );
+  }
+}
 
 export default Battle;
